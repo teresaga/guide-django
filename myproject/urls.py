61852739c15d9af6e4d2ecda232879b19151b08b
Teresa <teresagalaviza@gmail.com>
2019-08-21 13:35:07 -0600

    Advanced Concepts

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,5 +22,7 @@ from boards import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     url(r'^$', views.home, name="home"),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls)
 ]
